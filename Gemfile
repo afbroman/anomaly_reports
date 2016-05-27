ef78c82ab904319f9e0b123d299bcc3dff1a794b
Andrew Broman <abroman@medicine.wisc.edu>
2016-05-27 14:14:17 -0500

    Dump fake data to seeds.rb

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
   gem 'pry-rails'
+  gem 'faker'
+  gem 'seed_dump'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Report.create!([
+  {title: "Booked Import", date: "2016-05-12", source: "Booked"},
+  {title: "REGGIE Import", date: "2016-05-23", source: "REGGIE"},
+  {title: "REDCap Import", date: "2016-04-30", source: "REDCap"}
+])
+ReportEntry.create!([
+  {report_id: 1, first_name: "Kaelyn", last_name: "Kihn"},
+  {report_id: 1, first_name: "Alisha", last_name: "Gibson"},
+  {report_id: 1, first_name: "Murphy", last_name: "Bode"},
+  {report_id: 1, first_name: "Archibald", last_name: "Barrows"},
+  {report_id: 1, first_name: "Maxime", last_name: "Marquardt"},
+  {report_id: 1, first_name: "Katelynn", last_name: "Mayer"},
+  {report_id: 1, first_name: "Anderson", last_name: "Hane"},
+  {report_id: 1, first_name: "Charlotte", last_name: "Wiegand"},
+  {report_id: 1, first_name: "Georgette", last_name: "Herman"},
+  {report_id: 1, first_name: "Jacinthe", last_name: "Hettinger"},
+  {report_id: 1, first_name: "Jeffrey", last_name: "Corwin"},
+  {report_id: 1, first_name: "Mollie", last_name: "Bergstrom"},
+  {report_id: 1, first_name: "Orpha", last_name: "Satterfield"}
+])
